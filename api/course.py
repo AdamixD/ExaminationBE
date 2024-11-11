fd21cf4e9017236117490c23da0886d2bd5aee61
Michał Laskowski <01158164@pw.edu.pl>
2024-11-11 17:47:35 +0100

    Added Course api and service

---
+++ api/course.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from typing import List
+
+import services.course as service
+
+from database.session import get_db
+from schemas.course import CourseCreate, CourseResponse, CourseUpdate
+
+router = APIRouter(prefix="/courses", tags=["courses"])
+
+
+@router.get("/all", response_model=List[CourseResponse])
+def get_all_courses(db: Session = Depends(get_db)):
+    return service.get_all_courses(db=db)
+
+
+@router.get("/{course_id}", response_model=CourseResponse)
+def get_course(course_id: int, db: Session = Depends(get_db)):
+    course = service.get_course(db, course_id)
+    if course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+    return course
+
+
+@router.post("/", response_model=CourseResponse)
+def create_course(course: CourseCreate, db: Session = Depends(get_db)):
+    return service.create_course(db, course)
+
+
+@router.put("/{course_id}", response_model=CourseResponse)
+def update_course(course_id: int, course: CourseUpdate, db: Session = Depends(get_db)):
+    updated_course = service.update_course(db, course_id, course)
+    if updated_course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+    return updated_course
+
+
+@router.delete("/{course_id}", response_model=CourseResponse)
+def delete_course(course_id: int, db: Session = Depends(get_db)):
+    deleted_course = service.delete_course(db, course_id)
+    if deleted_course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+    return deleted_course
---
+++ services/course.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import Session
+
+from models.course import Course
+from schemas.course import CourseCreate, CourseUpdate
+
+def get_all_courses(db: Session):
+    return db.query(Course).all()
+
+
+def get_course(db: Session, course_id: int):
+    return db.query(Course).filter(Course.id == course_id).first()
+
+
+def create_course(db: Session, course: CourseCreate):
+    db_course = Course(
+        title=course.title,
+    )
+    db.add(db_course)
+    db.commit()
+    db.refresh(db_course)
+    return db_course
+
+
+def update_course(db: Session, course_id: int, course: CourseUpdate):
+    db_course = get_course(db, course_id)
+    if not db_course:
+        return None
+    for key, value in course.dict(exclude_unset=True).items():
+        setattr(db_course, key, value)
+    db.commit()
+    db.refresh(db_course)
+    return db_course
+
+
+def delete_course(db: Session, course_id: int):
+    db_course = get_course(db, course_id)
+    if db_course:
+        db.delete(db_course)
+        db.commit()
+    return db_course
